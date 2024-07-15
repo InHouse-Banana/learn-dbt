2592a72d0f037d3044ce260056b8e47ecbb3eb42
Anibal Gonzalez <anibal.gonzalez@contextlabs.com>
2024-07-15 21:27:16 +0200

    asdf

--- learn_dbt/models/example/playing_with_tests.sql
+++ learn_dbt/models/example/playing_with_tests.sql
@@ -1,4 +1,4 @@
  {{ config(materialized='table') }}
 
-select *
+select c_custkey, c_mktsegment, {{rename_segments('c_mktsegment')}} as mkt_segment_adjusted
 from snowflake_sample_data.tpch_sf1.customer
--- old_models/snowflake_customer_purchases.sql
+++
@@ -1,7 +0,0 @@
- {{ config(materialized='table') }}
-   
-   select c.c_custkey, c.c_name, c.c_nationkey as nation, sum(o.o_totalprice) as total_order_price
-     from snowflake_sample_data.tpch_sf1.customer as c
-left join snowflake_sample_data.tpch_sf1.orders as o
-       on c.c_custkey = o_custkey
- group by c.c_custkey, c.c_name, c.c_nationkey
